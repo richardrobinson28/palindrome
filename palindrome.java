e4fe351ad9d29e237c9bee0121a34b32fdcc1a70
richardrobinson28 <richardrobinson28@users.noreply.github.com>
2017-01-16 15:20:59 +0000

    Update palindrome.java
    Added comments, fixed bug in for loop, and optimised code

--- palindrome.java
+++ palindrome.java
@@ -1,29 +1,39 @@
 import java.util.Scanner;
 
-public class palindrome {
+public class palindrome2 {
 	public static void main(String args[]) {
+		// Initialises scanner for input and a string variable for input
 		Scanner in = new Scanner(System.in);
 		String word;
+
+		// Prints statement requesting user input then allows user input
 		System.out.print("Please enter word: ");
-		word = in.next();
-		System.out.println(check(word, reverse(word)));
+		word = in.next(); // Allows user input
+
+		// Prints out return statement check method after reversing user input
+		// and checking if it is a palindrome
+		System.out.println(check(word));
+
+		// Closes scanner input
 		in.close();
 	}
 
-	public static String reverse(String a) {
-		String reverse = "";
-		for (int i = (a.length() - 1); i >= 0; i--) {
-			reverse += a.charAt(i);
-		}
-		return reverse;
-	}
+	public static String check(String a) {
+		// Initialises integer variable for length of word
+		int len = a.length();
 
-	public static String check(String a, String b) {
-		for (int i = 0; i < (a.length() / 2);) {
-			if (!(a.charAt(i) == b.charAt(i)))
+		// For loop is set up to go through each character of the word from the
+		// start and compares the character at same position from the end of the
+		// word
+		for (int i = 0; i < (a.length() / 2); i++) {
+			// If statement checks if the statement of two characters equals each other is false
+			if (a.charAt(i) != a.charAt(len - 1 - i)){
+				// Returns a '[word] is not a palindrome' statement if neither character are the same
 				return a + " is not a palindrome";
-			break;
+			}
 		}
+
+		// If there no problems arise in the for loop, a '[word] is a palindrome' statement is returned
 		return a + " is a palindrome";
 	}
 }
